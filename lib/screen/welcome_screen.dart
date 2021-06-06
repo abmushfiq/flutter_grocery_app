41cb96f85a890e50a0ca272a211f5cd824aa2fac
abmushfiq <abmushfiq@gmail.com>
2021-06-06 15:40:50 +0530

    ui and auth

---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/provider/auth_provider.dart';
+import 'package:flutter_project/screen/onboard_screen.dart';
+import 'package:provider/provider.dart';
+
+class WelcomeScreen extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final auth = Provider.of<AuthProvider>(context);
+
+    bool _validphoneNumber = false;
+    var _phoneNumberController = TextEditingController();
+
+    void showBottomSheet (context) {
+      showModalBottomSheet(
+        context: context,
+        builder: (context)=> StatefulBuilder(builder: (context, StateSetter myState){
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  Text('Enter your phone number to proceed', style: TextStyle(fontSize: 12,color: Colors.grey),),
+                  SizedBox(height: 20,),
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixText: '+94',
+                      labelText: '9 digit mobile number',
+                    ),
+                    autofocus: true,
+                    keyboardType: TextInputType.phone,
+                    maxLength: 9,
+                    controller: _phoneNumberController,
+                    onChanged: (value){
+                      if(value.length==9){
+                        myState((){
+                          _validphoneNumber = true;
+                        });
+                      }else{
+                        myState((){
+                          _validphoneNumber = false;
+                        });
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AbsorbPointer(
+                          absorbing: _validphoneNumber ? false:true,
+                          child: FlatButton(
+                              color: _validphoneNumber ? Theme.of(context).primaryColor : Colors.grey,
+                              onPressed: (){
+                                    String number = '+94${_phoneNumberController.text}';
+                                    auth.verifyphone(context, number);
+                              },
+                              child: Text(_validphoneNumber ? 'CONTINUE' : 'ENTER PHONE NUMBER', style: TextStyle(color: Colors.white),)
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        ),
+      );
+
+    }
+
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0.0,
+              top:10.0,
+              child: FlatButton(
+                child: Text('SKIP', style: TextStyle(color: Colors.blue),),
+                onPressed: (){},
+              ),
+            ),
+            Column(
+              children: [
+                Expanded(child: OnBaordScreen()) ,
+                Text('Ready to order from your nearest shop?', style: TextStyle(color: Colors.grey),),
+                SizedBox(height: 20,),
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('SET DELIVERY LOCATION', style: TextStyle(color: Colors.white),),
+                  onPressed: (){},
+                ),
+                FlatButton(
+                    
+                    child:  RichText(text: TextSpan(
+                        text: ' Already a Customer ? ', style: TextStyle(color: Colors.grey),
+                        children: [
+                          TextSpan(
+                              text: 'Login', style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue)
+                          )
+                        ]
+                    ),
+
+                    ),
+                  onPressed: (){
+                      showBottomSheet(context);
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
